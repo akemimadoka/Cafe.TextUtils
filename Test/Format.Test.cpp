3fb83757b4279e50711f318cb4e156d37ae10150
akemimadoka <jxhzq1996@126.com>
2019-05-13 11:15:56 +0800

    更新运行时编码转换

--- Test/Format.Test.cpp
+++ Test/Format.Test.cpp
@@ -9,6 +9,6 @@ TEST_CASE("Cafe.TextUtils.Format", "[TextUtils][Format]")
 	SECTION("Formatting")
 	{
 		const auto formattedString = FormatString(CAFE_UTF8_SV("${0}, ${1}, ${3:x}, ${2}"), 1, 2.5f, -3, 18);
-		REQUIRE(formattedString.GetView() == CAFE_UTF8_SV("1, 2.50000, 12, -3"));
+		REQUIRE(formattedString == CAFE_UTF8_SV("1, 2.50000, 12, -3"));
 	}
 }
--- Test/Misc.Test.cpp
+++ Test/Misc.Test.cpp
@@ -1,5 +1,6 @@
 #include <Cafe/TextUtils/CodePointIterator.h>
 #include <catch2/catch.hpp>
+#include <cstring>
 
 using namespace Cafe;
 using namespace TextUtils;
@@ -20,12 +21,29 @@ TEST_CASE("Cafe.TextUtils.Misc", "[TextUtils][Misc]")
 		CHECK(read == end);
 	}
 
-	SECTION("String Convertion")
+	SECTION("String convertion")
 	{
 		const auto codePointString = EncodeTo<Encoding::CodePage::CodePoint>(CAFE_UTF8_SV("测试"));
 		REQUIRE(codePointString.GetSize() == 3);
 		CHECK(codePointString[0] == 0x6D4B);
 		CHECK(codePointString[1] == 0x8BD5);
 		CHECK(codePointString[2] == 0);
+	}
+
+	SECTION("Runtime string convertion")
+	{
+		const auto u8Str = CAFE_UTF8_SV("测试");
+		const auto resultVec = EncodeRuntime(Encoding::CodePage::Utf8, gsl::as_bytes(u8Str.GetSpan()),
+		                                     Encoding::CodePage::CodePoint);
+		REQUIRE(resultVec.size() == 3 * sizeof(Encoding::CodePointType));
+		Encoding::CodePointType codePoint;
+		std::memcpy(&codePoint, resultVec.data(), sizeof(Encoding::CodePointType));
+		CHECK(codePoint == 0x6D4B);
+		std::memcpy(&codePoint, resultVec.data() + sizeof(Encoding::CodePointType),
+		            sizeof(Encoding::CodePointType));
+		CHECK(codePoint == 0x8BD5);
+		std::memcpy(&codePoint, resultVec.data() + 2 * sizeof(Encoding::CodePointType),
+		            sizeof(Encoding::CodePointType));
+		CHECK(codePoint == 0);
 	}
 }
